555d0f549fc042e7d5830adffbbf159ca886483c
Maria Fernanda <mfdiazdelcastillo@gmail.com>
2024-01-13 11:30:31 -0500

    Documentado git config y git diff

--- git.ps1
+++ git.ps1
@@ -15,3 +15,21 @@ git init
 # git status 
 # Me permite conocer el estado de mi repositorio git 
 git status
+
+# git add
+# Me permite empezar a seguir los cambios de un archivo(s)
+git add git.ps1 # Voy a seguir los cambios de git.ps1
+
+# git commit
+# Me permite registrar los cambios de un archivo(s)
+git commit -m"Inicio de proyecto" # Registro un cambio (versión) con un mensaje
+
+# git config
+# Configurar parámetros de git 
+git config --global user.name "María Fernanda Diaz del Castillo Posso"
+git config --global user.name "mfdiazdelcastillo@gmail.com" 
+git config --list --global
+
+# git diff
+# Me permite visualizar los cambios realizados respecto a una versión anterior
+git diff 
