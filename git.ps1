6bbab909a116ec54363dc6a5e5549ebb1573533a
Maria Fernanda <mfdiazdelcastillo@gmail.com>
2024-01-13 11:00:30 -0500

    Inicio de proyecto

---
+++ git.ps1
@@ -0,0 +1,17 @@
+# git
+# Herramienta para controlar versionamientos
+#
+# Beneficios
+# - Establecer versiones de mi código para compararla en el tiempo
+# -- Conocer que ha cambiado en mi código, lo cual puede impactar en un error que
+#    se puede estar presentando facilitando la depuración del código 
+# -- Registrar las actividades que estoy ejecutando en el código 
+# - Centralizar y respaldar mi código
+
+# git init 
+# me permite activar la herramienta git en un proyecto(carpeta)
+git init
+
+# git status 
+# Me permite conocer el estado de mi repositorio git 
+git status
